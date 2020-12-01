37abf5368a80ad61b1e2dee8e86fb747e4a13eaf
gladiussdv <gladiussdv@gmail.com>
2020-12-01 08:39:52 +0200

    backend finished

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,8 +1,8 @@
-const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
+const admin = require('firebase-admin')
+admin.initializeApp()
+const db = admin.firestore()
 
 module.exports = {
-    admin,
-    db
+  admin,
+  db
 }
